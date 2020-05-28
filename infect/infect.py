5d8cc8917942e1d2f8cf584664d044eaaae14835
Ortimis <jakob@drortmann.de>
2020-05-28 12:20:17 +0200

    wolf sheep from mesa examples as well as first steps for infection model

--- infect/infect.py
+++ infect/infect.py
@@ -9,28 +9,37 @@ import matplotlib.pyplot as plot
 import numpy as np
 
 
-class MoneyModel(Model):
-    """A model with some number of agents."""
+class InfectModel(Model):
+    """A model with some number of agents and a patient 0"""
 
     def __init__(self, N, width, height):
         super()
         self.running = True
-        self.num_agents = N
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
+        self.num_innocent_agents = N
+        self.time_contagious = 7
+
         # Create agents
-        for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+        for i in range(self.num_innocent_agents):
+            a = HumanAgent(i + 1, self)
             self.schedule.add(a)
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
 
+        self.patient0 = HumanAgent(0, self)
+        self.schedule.add(self.patient0)
+
         self.datacollector = DataCollector(
-            model_reporters={"Gini": compute_gini},
-            agent_reporters={"Wealth": "wealth"}
+            model_reporters={"Gini": ""},
+            agent_reporters={
+                "Infected": "is_infected",
+                "Immune": "is_immune",
+                "Contagious": "is_contagious",
+            }
         )
 
     def step(self):
@@ -38,14 +47,16 @@ class MoneyModel(Model):
         self.schedule.step()
 
 
-class MoneyAgent(Agent):
-    """ An agent with fixed initial wealth.
-        It can move() and give_money()
-    """
+class HumanAgent(Agent):
+    """An agent with fixed initial wealth."""
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.wealth = 1
+        self.is_infected = False
+        self.is_immune = False
+        self.is_contagious = False
+        self.time_since_infection = 0
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
@@ -55,31 +66,41 @@ class MoneyAgent(Agent):
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
 
-    def give_money(self):
+    def spread_virus(self):
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) > 1:
-            other = self.random.choice(cellmates)
-            other.wealth += 1
-            self.wealth -= 1
+            for mate in cellmates:
+                mate.is_infected = True
 
     def step(self):
+
         self.move()
-        if self.wealth > 0:
-            self.give_money()
+
+        if self.is_infected == True:
+            self.time_since_infection += 1
+
+        if self.time_since_infection < self.model.time_contagious:
+            self.is_contagious = True
+        else:
+            self.is_contagious = False
+
+        if self.is_contagious == True:
+            self.spread_virus()
 
 
+"""
 def compute_gini(model):
     agent_wealths = [agent.wealth for agent in model.schedule.agents]
     x = sorted(agent_wealths)
     N = model.num_agents
     B = sum(xi * (N-i) for i, xi in enumerate(x)) / (N*sum(x))
     return (1 + (1/N) - 2*B)
+"""
 
-
-""" 
-model = MoneyModel(N=50, width=10, height=10)
+model = InfectModel(N=50, width=10, height=10)
 for i in range(100):
-    model.step() """
+    model.step()
+    print(model.schedule.agents)
 
 """ gini = model.datacollector.get_model_vars_dataframe()
 print(type(gini))
--- infect/infect_vis.py
+++ infect/infect_vis.py
@@ -1,32 +1,33 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.modules import ChartModule
 from mesa.visualization.ModularVisualization import ModularServer
-from gini import MoneyModel
+from infect import InfectModel
 
 
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle",
                  "Filled": "true",
-                 "r": 0.5}
+                 "r": 0.5,
+                 "Color": "grey",
+                 }
 
-    if agent.wealth > 0:
+    if agent.is_contagious:
         portrayal["Color"] = "red"
         portrayal["Layer"] = 0
-    else:
-        portrayal["Color"] = "grey"
+    if agent.is_immune:
+        portrayal["Color"] = "blue"
         portrayal["Layer"] = 1
-        portrayal["r"] = 0.2
     return portrayal
 
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
-chart = ChartModule([{"Label": "Gini",
+chart = ChartModule([{"Label": "Chart",
                       "Color": "Black"}],
                     data_collector_name='datacollector')
 
-server = ModularServer(MoneyModel,
+server = ModularServer(InfectModel,
                        [grid, chart],
                        "Money Model",
-                       {"N": 100, "width": 10, "height": 10})
-server.port = 8521  # The default
+                       {"N": 10, "width": 10, "height": 10})
+server.port = 8522  # The default
 server.launch()
